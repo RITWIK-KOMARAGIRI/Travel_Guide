12e68caddcaf0fc247476131ba9f4aaa82d5b761
RITWIK-KOMARAGIRI <komaragiriritwik@gmail.com>
2025-09-02 13:05:35 +0530

    Update Ruby code to open README.md in binary mode

---
+++ Travel_Guide.rb
@@ -0,0 +1,6 @@
+# Updated Ruby code to open README.md in binary mode
+
+# Example of the updated code:
+# File.open('README.md', 'rb')
+
+# Place the updated Ruby code here after identifying occurrences.
